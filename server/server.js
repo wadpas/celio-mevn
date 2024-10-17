001b1dd301951e2e50e8fa2d2e736656689e0321
wadpas <wadpas@gmail.com>
2024-10-17 21:40:41 +0300

    markup home page done

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const connectDB = require('./config/db')
+const mongoose = require('mongoose')
 const path = require('path')
-
 const app = express()
 
-connectDB()
 app.use(cors())
 app.use(express.json({ extended: false }))
 
@@ -21,8 +20,17 @@ if (process.env.NODE_ENV === 'production') {
 	})
 }
 
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 5000
+const start = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_URI)
+		console.log('MongoDB connected')
+		app.listen(port, () => {
+			console.log('Server started on port 5000')
+		})
+	} catch (error) {
+		console.log(error)
+	}
+}
 
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`)
-})
+start()
